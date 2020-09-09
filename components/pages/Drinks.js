086a206fcf868ad7e7eee3f575ce23c071dc00e7
Andy Cousineau <andrew.cousineau@gmail.com>
2020-09-09 17:43:43 -0400

    Drinks finalization

--- components/pages/Drinks.js
+++ components/pages/Drinks.js
@@ -11,24 +11,56 @@ const Drinks = () => {
       <Container>
         <section className="drink-section">
           <h3>Signature Cocktails</h3>
-          <ul>
-            <li>Finn and Tonic</li>
-            <li>Blueberry Margarita</li>
+          <ul className="cocktails">
+            <li>Finn &amp; Tonic</li>
+            <li>The Martharita</li>
+            <caption>Blueberry Margarita</caption>
           </ul>
+          <br />
+          <caption>* Other cocktails available upon request</caption>
         </section>
         <section className="drink-section">
           <h3>Beer</h3>
-          <ul>
-            <li>IPAs for Daze</li>
-            <li>Lagers</li>
-            <li>Belgians</li>
+          <ul className="beers">
+            <li>
+              <span className="beer">
+                <strong>One Hop This Time: Galaxy</strong>
+                <span className="abv">6% ABV IPA</span>
+              </span>
+              <span className="brewery">Night Shift Brewing</span>
+            </li>
+            <li>
+              <span className="beer">
+                <strong>Whirlpool</strong>
+                <span className="abv">4.5% ABV NE Pale Ale</span>
+              </span>
+              <span className="brewery">Night Shift Brewing</span>
+            </li>
+            <li>
+              <span className="beer">
+                <strong>Nite Lite</strong>
+                <span className="abv">4.3% ABV Light Lager</span>
+              </span>
+              <span className="brewery">Night Shift Brewing</span>
+            </li>
+            <li>
+              <span className="beer">
+                <strong>PILS</strong>
+                <span className="abv">5.2% ABV Pilsner</span>
+              </span>
+              <span className="brewery">Lord Hobo Brewing Co.</span>
+            </li>
           </ul>
         </section>
         <section className="drink-section">
           <h3>Wine</h3>
           <ul>
-            <li>Chateau deux POPE</li>
-            <li>WHITE WINE Y'ALL</li>
+            <li>
+              <strong>Mon Frère</strong>, Pinot Noir, California
+            </li>
+            <li>
+              <strong>Planeta</strong>, Chardonnay, Italy
+            </li>
           </ul>
         </section>
       </Container>
